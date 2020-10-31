de20eb28fc555c7434502830b18f25ad44eceb5c
vippivivpiv <nguyendinhkhanh1997@gmail.com>
2020-10-31 09:30:43 +0700

    Before Chẹck Mouse pos

--- Assets/testtttttDiemVaDaGiac.cs
+++ Assets/testtttttDiemVaDaGiac.cs
@@ -1,21 +1,46 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
 public class testtttttDiemVaDaGiac : MonoBehaviour
 {
+   public  Camera main;
     public Transform[] point;
+    public Vector3 mousePosition;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+       // Debug.Log(Input.mousePosition);
+        if ( Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(Input.mousePosition);
+            Debug.Log(main.ScreenToWorldPoint(Input.mousePosition));
+            mousePosition = main.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = 0;
+
+            Debug.Log(mousePosition);
+            Calculate();
+        }
+    }
+
+    private void Calculate()
+    {
+        float angle = 0;
+
+        for (int i = 0; i < point.Length-1; i++)
+        {
+            angle += Vector3.Angle(point[i + 1].position - mousePosition, point[i].position - mousePosition);
+            Debug.Log(angle);
+        }
+        angle += Vector3.Angle(point[0].position - mousePosition, point[point.Length-1].position - mousePosition);
+        Debug.Log(angle);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(mousePosition, 0.5f);
     }
 }
